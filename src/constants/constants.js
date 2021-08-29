0688cab6831bdfc1baa38528db0dbd67edadda2d
Joe Thornton <joe.clifford.thornton@gmail.com>
2021-08-29 11:28:23 +0100

    Added genki-flashcards to the projects.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,6 +8,16 @@ export const projects = [
     source: 'https://github.com/FlyLikeAPenguin/React-Portfolio',
     visit: 'https://flylikeapenguin.github.io/React-Portfolio/',
     id: 0,
+  },
+  {
+    title: 'React Flashcards',
+    description: "A react webapp companion to the genki textbooks for Kanji/Kana.",
+    objectives: ["A fully featured flashcards app.", "User's lesson selection persists between sessions.", "Mobile friendly."],
+    image: './images/2.png',
+    tags: ['React.js', 'JSON', 'Node'],
+    source: 'https://github.com/FlyLikeAPenguin/Genki-Flashcards',
+    visit: 'https://flylikeapenguin.github.io/Genki-Flashcards/',
+    id: 1,
   }
 ];
 
@@ -17,4 +27,5 @@ export const TimeLineData = [
   { year: 2018, text: 'Started work at LabLogic Systems as a C# .Net Developer', },
   { year: 2020, text: 'LabLogic - Designed, Managed & Completed .Net Core REST API', },
   { year: 2021, text: 'Created Personal Portfolio using React.js & Next.js', },
+  { year: 2021, text: 'Created Genki Flashcards using React.js', },
 ];
